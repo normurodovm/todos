884d9e757e441473d897c494d0c8295b827fa815
normurodovm <nmaqsud302@gmail.com>
2024-09-08 21:21:12 +0500

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 // import { getData } from "./services/query/get-todo";
 // import { Card } from "@/components/todo-card";
 
-// export default async function Home() {
-//   const data = await getData();
+export default async function Home() {
+  // const data = await getData();
 
-//   return (
-//     <main className="p-2 flex flex-wrap justify-center">
-//       {data.map((todo) => (
-//         <Card key={todo.id} {...todo} />
-//       ))}
-//     </main>
-//   );
-// }
+  return (
+    <main className="p-2 flex flex-wrap justify-center">
+      {/* {data.map((todo) => (
+        <Card key={todo.id} {...todo} />
+      ))} */}
+    </main>
+  );
+}
