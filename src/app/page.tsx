ae839977b898dbcee9719f4a67b898d5413f34a7
normurodovm <nmaqsud302@gmail.com>
2024-09-08 21:38:57 +0500

    message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-// import { getData } from "./services/query/get-todo";
-// import { Card } from "@/components/todo-card";
+import { getData } from "./services/query/get-todo";
+import { Card } from "@/components/todo-card";
 
 export default async function Home() {
-  // const data = await getData();
+  const data = await getData();
 
   return (
     <main className="p-2 flex flex-wrap justify-center">
-      {/* {data.map((todo) => (
+      {data.map((todo) => (
         <Card key={todo.id} {...todo} />
-      ))} */}
+      ))}
     </main>
   );
 }
--- src/app/services/mutation/delete-todo.ts
+++ src/app/services/mutation/delete-todo.ts
@@ -1,19 +1,19 @@
-// "use server"
-// const url = process.env.APP_URL
+"use server"
+const url = process.env.APP_URL
 
-// import { revalidateTag } from "next/cache"
+import { revalidateTag } from "next/cache"
 
 
 
-// export const deletTodo  = async (id:number) => {
-//     try {
-//         const res = await fetch(`${url}/todos${id}`,{method:"DELETE"})
-//         const data = await res.json()
-//         return data;
-//       } catch (error) {
-//         const err = (error as Error).message;
-//         throw new Error(err);
-//       }finally{
-//         revalidateTag("todos");
-//       }
-// }
+export const deletTodo  = async (id:number) => {
+    try {
+        const res = await fetch(`${url}/todos${id}`,{method:"DELETE"})
+        const data = await res.json()
+        return data;
+      } catch (error) {
+        const err = (error as Error).message;
+        throw new Error(err);
+      }finally{
+        revalidateTag("todos");
+      }
+}
