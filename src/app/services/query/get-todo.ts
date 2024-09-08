105eca1354f348ad3a934c492b77350593679b07
normurodovm <nmaqsud302@gmail.com>
2024-09-08 21:17:24 +0500

    message

--- src/app/services/query/get-todo.ts
+++ src/app/services/query/get-todo.ts
@@ -1,27 +1,27 @@
-"use server"
+// "use server"
 
 
-const url = process.env.APP_URL;
+// const url = process.env.APP_URL;
 
 
-interface dataType {
-  title:string;
-  description:string;
-  id:number;
-}
+// interface dataType {
+//   title:string;
+//   description:string;
+//   id:number;
+// }
 
 
 
 
-export const getData = async () :Promise<dataType[]> => {
-  try {
-    const res = await fetch(`${url}/todos`, { next: { tags: ["todos"] } });
-    const data = await res.json()
-    return data;
-  } catch (error) {
-    const err = (error as Error).message;
-    throw new Error(err);
-  }
-};
+// export const getData = async () :Promise<dataType[]> => {
+//   try {
+//     const res = await fetch(`${url}/todos`, { next: { tags: ["todos"] } });
+//     const data = await res.json()
+//     return data;
+//   } catch (error) {
+//     const err = (error as Error).message;
+//     throw new Error(err);
+//   }
+// };
 
 
